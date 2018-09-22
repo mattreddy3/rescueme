0c71a53b7eac9ab79bc8d19e928dc28cbc697552
Matthew Reddy <matthewreddy@Matthews-MacBook-Pro-2.local>
2018-09-22 12:45:18 +0200

    adding nginx default proxy settings

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
       .catch(err=>console.log(err));
   }
   callApi = async() => {
-    const response = await fetch(`/hello`);
+    const response = await fetch(`/api/hello`);
     const body = await response.json();
     if(response.status !== 200) throw Error(body.message);
 
